b03b3a7cfd4565c0053a3b289cde545d9bb12292
Jensen <jensenhuang2@gmail.com>
2020-08-04 00:52:17 +0800

    video

---
+++ camera test code/opencamera.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+cam = cv2.VideoCapture(0)
+
+if not cam.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cam.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # show the img
+    cv2.imshow('frame', frame)
+    # press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# release
+cam.release()
+cv2.destroyAllWindows()
+#test
+
+# In[ ]:
+
+
+
+
